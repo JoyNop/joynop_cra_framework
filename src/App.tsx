fe88611a4d212c1cc79011654ef617dc9a09e954
HanRui <597589373@qq.com>
2019-10-30 10:30:53 +0800

    更新antd 相关

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
-
+import { Button } from "antd";
 const App: React.FC = () => {
-  return <div className="App">hello world</div>;
+  const openMenu = () => {
+    debugger;
+    console.log(1);
+  };
+  return (
+    <div className="App">
+      hello world
+      <button onClick={openMenu}>hello</button>
+      <Button type="primary">But9999ton</Button>
+    </div>
+  );
 };
 
 export default App;
